5cf16fc1dc0020f30f09f5727905ae53dc58db2f
Drshoboat <drshoboat@gmail.com>
2018-08-28 21:20:45 -0500

    Work from home

--- Stack Labs/SyntaxChecker.java
+++ Stack Labs/SyntaxChecker.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+import java.io.File;
+import java.util.Stack;
 public class SyntaxChecker {
+    public static void main(String args[]) throws FileNotFoundException {
+      File file = new File("syntaxChecker.dat");
+      Scanner sc = new Scanner(file);
+      Stack<String> syntax = new Stack<String>();
+      String bob = sc.nextLine();
 
-
-
-
-
+      System.out.println(syntax);
+    }
 }
