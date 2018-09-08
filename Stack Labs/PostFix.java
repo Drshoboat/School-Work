4778d3fcf88f0196a195cd3bc8b5add7d0fbdaa2
Drshoboat <drshoboat@gmail.com>
2018-09-07 20:31:22 -0500

    ITS FINALLY DONE

--- Stack Labs/PostFix.java
+++ Stack Labs/PostFix.java
@@ -6,30 +6,37 @@ public class PostFix {
   public float solvePost(String[] arr) throws NumberFormatException{
     String numbers = "0123456789";
     float result = 0;
-    for(int i = -1; i < arr.length; i++) {
+    nums.push(Float.parseFloat(arr[0]));
+    for(int i = 1; i < arr.length; i++) {
 
       if(nums.size() > 0){
       //  float num1 = nums.pop();
         //float num2 = nums.pop();
       if(numbers.indexOf(arr[i]) > -1) {
         nums.push(Float.parseFloat(arr[i]));
-      //  System.out.println(nums.peek());
+        //System.out.println(nums.peek());
     } else if(arr[i].equals("+")) {
         result = nums.pop() + nums.pop();
-        System.out.println(result);
-        //nums.push(result);
+        //System.out.println(result);
+        nums.push(result);
     } else if(arr[i].equals("-")){
-        result = nums.pop() - nums.pop();
-        //nums.push(result);
+        float num1 = nums.pop();
+        float num2 = nums.pop();
+        result = num2 - num1;
+        //System.out.println(result);
+        nums.push(result);
     } else if(arr[i].equals("*")) {
         result = nums.pop() * nums.pop();
-        //nums.push(result);
+        //System.out.println(result);
+        nums.push(result);
     } else if(arr[i].equals("/")) {
-        result = nums.pop() / nums.pop();
-        //nums.push(result);
+      float num1 = nums.pop();
+      result = nums.pop() / num1;
+        //System.out.println(result);
+        nums.push(result);
     }
   }
-    nums.push(result);
+    //nums.push(result);
     }
     return nums.pop();
   }
--- Stack Labs/PostfixRunner.java
+++ Stack Labs/PostfixRunner.java
@@ -1,18 +1,18 @@
 public class PostfixRunner {
     public static void main(String args[]) throws NumberFormatException {
       PostFix bob = new PostFix();
-      String[] first = new String[] {"2", "7", "+", "1", "2", "+", "+"};
-      String[] second = new String[] {"1", "2", "3", "4", "+" , "+", "+"};
-      String[] third = new String[] {"9", "3", "*", "8", "/", "4", "+"};
-      String[] fourth = new String[] {"3", "3", "+", "7", "*", "9", "2", "/","+"};
-      String[] fifth = new String[] {"9", "3", "/", "2", "*", "7", "9", "*", "+", "4", "–"};
-      String[] sixth = new String[] {"5", "5", "+", "2", "*", "4", "/","9", "+"};
-      System.out.println(bob.solvePost(first));
-    /*  System.out.println(bob.solvePost(second));
-      System.out.println(bob.solvePost(third));
-      System.out.println(bob.solvePost(fourth));
-      System.out.println(bob.solvePost(fifth));
-      System.out.println(bob.solvePost(sixth));
-      */
+       String[] first = new String[] {"2", "7", "+", "1", "2", "+", "+"};
+       String[] second = new String[] {"1", "2", "3", "4", "+" , "+", "+"};
+       String[] third = new String[] {"9", "3", "*", "8", "/", "4", "+"};
+       String[] fourth = new String[] {"3", "3", "+", "7", "*", "9", "2", "/","+"};
+       String[] fifth = new String[] {"9", "3", "/", "2", "*", "7", "9", "*", "+", "4", "-"};
+       String[] sixth = new String[] {"5", "5", "+", "2", "*", "4", "/","9", "+"};
+       System.out.println(bob.solvePost(first));
+       System.out.println(bob.solvePost(second));
+       System.out.println(bob.solvePost(third));
+       System.out.println(bob.solvePost(fourth));
+       System.out.println(bob.solvePost(fifth));
+       System.out.println(bob.solvePost(sixth));
+
     }
 }
