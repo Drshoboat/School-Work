f314c18ee8ee6aa78d280e3a744fbe4cc8c2ac8a
Drshoboat <drshoboat@gmail.com>
2018-10-19 08:52:31 -0500

    finshed bidirectional graph lab

--- Data_Structure CompSciIII/Graph Labs/BiDirectionalGraph.java
+++ Data_Structure CompSciIII/Graph Labs/BiDirectionalGraph.java
@@ -11,27 +11,76 @@ import java.util.Set;
 import java.util.TreeSet;
 import java.util.Scanner;
 import static java.lang.System.*;
+import java.util.*;
 
 public class BiDirectionalGraph
 {
-	private TreeMap<String, TreeSet<String>> map;
-	private boolean yahOrNay;
+	Map<String, String> biGraph = new TreeMap<String, String>();
 
-	public BiDirectionalGraph(String line)
+	public BiDirectionalGraph(){
+
+	}
+	public void addToMap(String points){
+       //System.out.println(points);
+       //splits data into pairs
+       String[] point= points.split(" ");//splits list of names into indiv. names
+
+        if(biGraph.containsKey(point[0])) {
+          biGraph.replace(point[0], biGraph.get(point[0]) + " " + point[1]);
+        }
+        if(!biGraph.containsKey(point[0])) {
+          //System.out.println(point.length);
+          biGraph.put(point[0], point[1]);
+        }
+        if(biGraph.containsKey(point[1])) {
+          biGraph.replace(point[1], biGraph.get(point[1]) +  " " + point[0]);
+
+        }
+        if(!biGraph.containsKey(point[1])) {
+          biGraph.put(point[1],point[0]);
+        }
+
+
+    //System.out.println(biGraph);
+  }
+
+  public boolean contains(String letter)
 	{
+    return biGraph.containsKey(letter);
 	}
 
-	public boolean contains(String name)
-	{
-		return true;
-	}
+  public boolean check(String first, String second, ArrayList<String> been) {
+    //System.out.println(first + " " + second);
+    //System.out.println(biGraph.toString());
 
-	public void check(String first, String second, TreeSet<String> placedUsed)
-	{
-	}
+    if(!contains(first)) {
+      //System.out.println("you shouldn't be seeing this yet bud" + first);
+      return false;
+    }
+    if(been.contains(first)) {
+      //System.out.println("you shouldn't be seeing this yet bud" + first);
+      return false;
+    }
+    String[] seconds = biGraph.get(first).split(" ");
+    for(int i = 0; i < seconds.length; i++) {
+      if((seconds[i]).equals(second)){
+        //System.out.println("you did it");
 
-	public String toString()
-	{
-		return "";
-	}
+        return true;
+      } else {
+        been.add(first);
+        if(check(seconds[i], second, been)) {
+          //System.out.println("you did it");
+
+          return true;
+        }
+      }
+
+    }
+    return false;
+  }
+
+    public String toString() {
+      return " ";
+    }
 }
--- Data_Structure CompSciIII/Graph Labs/BiDirectionalGraphRunner.java
+++ Data_Structure CompSciIII/Graph Labs/BiDirectionalGraphRunner.java
@@ -8,10 +8,36 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.TreeSet;
 import static java.lang.System.*;
+import java.util.*;
 
 public class BiDirectionalGraphRunner
 {
 	public static void main( String[] args ) throws IOException
 	{
+		File file = new File("bidgraph.dat");//reads in data from .dat file
+		Scanner f = new Scanner(file);
+
+		int numTimes = f.nextInt();
+		f.nextLine();
+
+		for(int i = 0; i < numTimes; i++) {
+			BiDirectionalGraph jimmybillyandmillie = new BiDirectionalGraph();
+			String names = f.nextLine(); // makes a string of all the names
+			String whatToConnect = f.nextLine(); // makes a string of names to connect
+			String[] name = names.split(" "); //seperates each name into an array
+
+			for(int j = 0; j < name.length; j+=2) {
+				jimmybillyandmillie.addToMap(name[j] + " " + name[j+1]);
+			}
+
+			String[] connections = whatToConnect.split(" ");
+			ArrayList<String> timmyandjimmy = new ArrayList<String>();
+
+			if(jimmybillyandmillie.check(connections[0], connections[1], timmyandjimmy)) {
+				System.out.println(connections[0] + " CONNECTS TO " + connections[1] + " == YAH");
+			} else {
+				System.out.println(connections[0] + " CONNECTS TO " + connections[1] + " == NAH");
+			}
+		}
 	}
 }
