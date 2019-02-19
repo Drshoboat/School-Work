d33212b1546e5a9c099a19c4a90f9e796e0f61a6
Drshoboat <drshoboat@gmail.com>
2019-02-19 16:27:46 -0600

    dumb math

---
+++ Data_Structure CompSciIII/DumbMath.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class DumbMath {
+   public static void main(String[] args){
+      int my_num = 69420;
+      long my_new_num = (long)Math.pow(69420,69420);
+      System.out.println(my_new_num);
+   }
+}
